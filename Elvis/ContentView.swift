9907a3b9886f6be3561f8b4538f84205177b2e6f
Christopher Kennedy <kennednu@bc.edu>
2023-01-30 17:23:40 -0500

    Elvis solution

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Whats So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.purple)
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300.0, height: 300.0)
+                .foregroundColor(.black)
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.purple
+                )
+            Spacer()
+            
+            HStack{
+                Button("Peace"){
+                    messageString = "Peace"
+                    imageName = "peacesign"
+                }
+                Button("Love"){
+                    messageString = "Love"
+                    imageName = "heart"
+                }
+                Button("Understanding"){
+                    messageString = "Understanding"
+                    imageName = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
+
         }
         .padding()
     }

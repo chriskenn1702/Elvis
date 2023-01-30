d91a6dff21871768ad0a0ca8e06a9928239451b1
Christopher Kennedy <kennednu@bc.edu>
2023-01-30 17:07:35 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Christopher Kennedy on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
